b6938ec1003ea17fe56b82fd9565acac95e58b4a
3nr1c096 <enrico-donato@virgilio.it>
2017-08-17 16:47:31 +0200

    Add files via upload

---
+++ PrimeFactors.java
@@ -0,0 +1,26 @@
+package skill.test;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PrimeFactors {
+	static int[] v = new int[0];
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please insert a number: ");
+		int n = sc.nextInt();
+		sc.close();
+		int x = n / 2;
+		for(int i = 2; i <= x; i++)
+			if(n % i == 0) {
+				v = Arrays.copyOf(v, v.length + 1);
+				v[v.length - 1] = i;
+				n /= i;
+				if(n == 1)
+					break;
+				i--;
+			}
+		System.out.println(Arrays.toString(v));
+	}//main
+}//PrimeFactors
