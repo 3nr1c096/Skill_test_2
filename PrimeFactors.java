d9006d659eb1fc2ae891796c8d335c51df9c2cb4
3nr1c096 <enrico-donato@virgilio.it>
2017-08-17 16:50:11 +0200

    Update PrimeFactors.java

--- PrimeFactors.java
+++ PrimeFactors.java
@@ -3,15 +3,14 @@ package skill.test;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class PrimeFactors {
-	static int[] v = new int[0];
-	
+public class PrimeFactors {	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Please insert a number: ");
 		int n = sc.nextInt();
 		sc.close();
 		int x = n / 2;
+		int[] v = new int[0];
 		for(int i = 2; i <= x && n > 1; i++)
 			if(n % i == 0) {
 				v = Arrays.copyOf(v, v.length + 1);
